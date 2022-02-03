52dfa66ef7dff2572b4150e2a2db49419e573201
the-satyam <satyamvishwa03@gmail.com>
2022-02-03 12:23:16 +0530

    app bar, boady, floating action button

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-      MaterialApp(
-        home: Text("Welcome to flutter"),
-      )
-  );
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+          title: Text(
+            'welcome to appbar',
+            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true),
+      body: Center(child: Text('Hello World')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+      child:  Text('Click Me'),
+      ),
+    ),
+  ));
 }
