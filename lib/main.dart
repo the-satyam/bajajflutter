ccd84b50fd79a8d5dfdf42f6bca5f0ed47270f63
the-satyam <satyamvishwa03@gmail.com>
2022-02-03 12:37:58 +0530

    body styling

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,23 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-          title: Text(
-            'welcome to appbar',
-            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-          ),
-          centerTitle: true),
-      body: Center(child: Text('Hello World')),
+        title: Text(
+          'welcome to appbar',
+          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.orange,
+      ),
+      body: Center(child: Text('Hello World',
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 35.0,
+        color: Colors.grey
+      ),)),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-      child:  Text('Click Me'),
+        child: Text('Click Me'),
+        backgroundColor: Colors.orange,
       ),
     ),
   ));
